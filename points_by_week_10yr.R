00bd6cbe02958e2d5540254cd4ca43ede15d5161
Nicholas Pylypiw <nick.pylypiw@gmail.com>
2019-11-11 11:46:56 -0500

    Update points_by_week_10yr.R
    update city wins title 2019

--- points_by_week_10yr.R
+++ points_by_week_10yr.R
@@ -1,6 +1,8 @@
 # create graph showing point tally by week for last 10 champions
 points.by.week.10yr <- 
   plot_ly(pbw.data.10yr, x = ~Week) %>%
+  add_trace(y = ~ManCity1819, name = 'Man City 2019', type = 'scatter', mode = 'lines',
+            line = list(color = 'rgba(220, 220, 220, 1)', width = 1)) %>%
   add_trace(y = ~ManCity1718, name = 'Man City 2018', type = 'scatter', mode = 'lines',
             line = list(color = 'rgba(220, 220, 220, 1)', width = 1)) %>%
   add_trace(y = ~Chelsea1617, name = 'Chelsea 2017', type = 'scatter', mode = 'lines',
@@ -19,7 +21,7 @@ points.by.week.10yr <-
             line = list(color = 'rgba(220, 220, 220, 1)', width = 1)) %>%
   add_trace(y = ~Chelsea0910, name = 'Chelsea 2010', type = 'scatter', mode = 'lines',
             line = list(color = 'rgba(220, 220, 220, 1)', width = 1)) %>%
-  add_trace(y = ~Liverpool1819, name = 'Liverpool 2019', type = 'scatter', mode = 'lines', 
+  add_trace(y = ~Liverpool1920, name = 'Liverpool 2020', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(200, 12, 46)', width = 4)) %>%
   
     layout(title = "Premier League Champions by Week <br> Last Ten Years",
