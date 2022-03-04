57e81ebac2615beba60e2fee8c0648fd89086549
Nicholas Pylypiw <nick.pylypiw@gmail.com>
2022-03-04 08:10:45 -0500

    update bruh

--- visuals/predicted_points_by_week.R
+++ visuals/predicted_points_by_week.R
@@ -8,6 +8,8 @@ predicted.points.by.week <-
             line = list(shape = 'spline', color = 'rgb(149,191,229)', width = 1)) %>%
   # add_trace(y = ~Bournemouth, name = 'Bournemouth', type = 'scatter', mode = 'lines', 
   #           line = list(shape = 'spline', color = 'rgb(181, 14, 18)', width = 1)) %>%
+  add_trace(y = ~Brentford, name = 'Brentford', type = 'scatter', mode = 'lines', 
+            line = list(shape = 'spline', color = 'rgb(227, 9, 16)', width = 1)) %>%
   add_trace(y = ~Brighton, name = 'Brighton', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(0, 87, 184)', width = 1)) %>%
   add_trace(y = ~Burnley, name = 'Burnley', type = 'scatter', mode = 'lines', 
@@ -20,8 +22,8 @@ predicted.points.by.week <-
             line = list(shape = 'spline', color = 'rgb(27, 69, 143)', width = 1)) %>%
   add_trace(y = ~Everton, name = 'Everton', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(39, 68, 136)', width = 1, dash = 'dot')) %>%
-  add_trace(y = ~Fulham, name = 'Fulham', type = 'scatter', mode = 'lines',
-            line = list(shape = 'spline', color = 'rgb(0, 0, 0)', width = 1)) %>%
+  # add_trace(y = ~Fulham, name = 'Fulham', type = 'scatter', mode = 'lines',
+  #           line = list(shape = 'spline', color = 'rgb(0, 0, 0)', width = 1)) %>%
   # add_trace(y = ~Huddersfield, name = 'Huddersfield', type = 'scatter', mode = 'lines', 
   #           line = list(shape = 'spline', color = 'rgb(14, 99, 173)', width = 1)) %>%
   add_trace(y = ~Leeds, name = 'Leeds United', type = 'scatter', mode = 'lines', 
@@ -36,36 +38,25 @@ predicted.points.by.week <-
             line = list(shape = 'spline', color = 'rgb(218, 41, 28)', width = 1)) %>%
   add_trace(y = ~Newcastle, name = 'Newcastle', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(45, 41, 38)', width = 1, dash = 'dash')) %>%
-  # add_trace(y = ~Norwich, name = 'Norwich City', type = 'scatter', mode = 'lines', 
-  #           line = list(shape = 'spline', color = 'rgb(0, 166, 80)', width = 1, dash = 'dash')) %>%
-  add_trace(y = ~SheffieldUnited, name = 'Sheffield United', type = 'scatter', mode = 'lines', 
-            line = list(shape = 'spline', color = 'rgb(238,39,55)', width = 1, dash = 'dash')) %>%
+  add_trace(y = ~Norwich, name = 'Norwich City', type = 'scatter', mode = 'lines',
+            line = list(shape = 'spline', color = 'rgb(0, 166, 80)', width = 1, dash = 'dash')) %>%
+  # add_trace(y = ~SheffieldUnited, name = 'Sheffield United', type = 'scatter', mode = 'lines', 
+  #           line = list(shape = 'spline', color = 'rgb(238,39,55)', width = 1, dash = 'dash')) %>%
   add_trace(y = ~Southampton, name = 'Southampton', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(215, 25, 32)', width = 1, dash = 'dash')) %>%
   add_trace(y = ~Tottenham, name = 'Tottenham', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(19, 34, 87)', width = 1)) %>%
-  # add_trace(y = ~Watford, name = 'Watford', type = 'scatter', mode = 'lines', 
-  #           line = list(shape = 'spline', color = 'rgb(251, 238, 35)', width = 1)) %>%
-  add_trace(y = ~WestBrom, name = 'West Bromwich Albion', type = 'scatter', mode = 'lines', 
-            line = list(shape = 'spline', color = 'rgb(18, 47, 103)', width = 1)) %>%
+  add_trace(y = ~Watford, name = 'Watford', type = 'scatter', mode = 'lines',
+            line = list(shape = 'spline', color = 'rgb(251, 238, 35)', width = 1)) %>%
+  # add_trace(y = ~WestBrom, name = 'West Bromwich Albion', type = 'scatter', mode = 'lines', 
+  #           line = list(shape = 'spline', color = 'rgb(18, 47, 103)', width = 1)) %>%
   add_trace(y = ~WestHam, name = 'West Ham United', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(122, 38, 58)', width = 1)) %>%
   add_trace(y = ~Wolves, name = 'Wolverhampton', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(253, 185, 19)', width = 1)) %>%
   
-  add_annotations(x = 0, y = 100, text = 'Manchester City 2017/2018 = 100 points', xref = 'x', yref = 'y', showarrow = F, xanchor = 'left',
-                  font = list(size = 16)) %>%
-  add_annotations(x = 0, y = 87, text = 'Average points by champion = 87 points', xref = 'x', yref = 'y', showarrow = F, xanchor = 'left',
-                  font = list(size = 16)) %>%
-  add_annotations(x = 0, y = 75, text = 'Manchester United 1996/1997 = 75 points', xref = 'x', yref = 'y', showarrow = F, xanchor = 'left',
-                  font = list(size = 16)) %>%
-  add_annotations(x = 0, y = 34, text = 'West Bromwich 2005/2006 = 34 points', xref = 'x', yref = 'y', showarrow = F, xanchor = 'left',
-                  font = list(size = 16)) %>%
-  add_annotations(x = 0, y = 42, text = 'West Ham 2002/2003 = 42 points', xref = 'x', yref = 'y', showarrow = F, xanchor = 'left',
-                  font = list(size = 16)) %>%
-  
   layout(
-    # title = paste("Premier League Position by Week <br>", max(rby.data$Year), "-", max(rby.data$Year)+1, '<br>', 'Projected as of Week ', max(pbw.data$Week)),
+    title = paste("Premier League Position by Week <br>", max(rby.data$Year), "-", max(rby.data$Year)+1, '<br>', 'Projected as of Week ', max(pbw.data$Week)),
          legend = list(font = list(size = 14)),
          xaxis = list(title = "Week", rangemode = 'tozero', showline = FALSE, zeroline = FALSE,
                       titlefont = list(size = 18),
--- visuals/rank_by_week.R
+++ visuals/rank_by_week.R
@@ -9,6 +9,8 @@ rank.by.week <-
             line = list(shape = 'spline', color = 'rgb(149,191,229)', width = 1)) %>%
   # add_trace(y = ~Bournemouth, name = 'Bournemouth', type = 'scatter', mode = 'lines', 
   #           line = list(shape = 'spline', color = 'rgb(181, 14, 18)', width = 1)) %>%
+  add_trace(y = ~Brentford, name = 'Brentford', type = 'scatter', mode = 'lines', 
+            line = list(shape = 'spline', color = 'rgb(227, 9, 16)', width = 1)) %>%
   add_trace(y = ~Brighton, name = 'Brighton', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(0, 87, 184)', width = 1)) %>%
   add_trace(y = ~Burnley, name = 'Burnley', type = 'scatter', mode = 'lines', 
@@ -21,8 +23,8 @@ rank.by.week <-
             line = list(shape = 'spline', color = 'rgb(27, 69, 143)', width = 1)) %>%
   add_trace(y = ~Everton, name = 'Everton', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(39, 68, 136)', width = 1, dash = 'dot')) %>%
-  add_trace(y = ~Fulham, name = 'Fulham', type = 'scatter', mode = 'lines',
-            line = list(shape = 'spline', color = 'rgb(0, 0, 0)', width = 1)) %>%
+  # add_trace(y = ~Fulham, name = 'Fulham', type = 'scatter', mode = 'lines',
+  #           line = list(shape = 'spline', color = 'rgb(0, 0, 0)', width = 1)) %>%
   # add_trace(y = ~Huddersfield, name = 'Huddersfield', type = 'scatter', mode = 'lines', 
   #           line = list(shape = 'spline', color = 'rgb(14, 99, 173)', width = 1)) %>%
   add_trace(y = ~Leeds, name = 'Leeds United', type = 'scatter', mode = 'lines', 
@@ -37,18 +39,18 @@ rank.by.week <-
             line = list(shape = 'spline', color = 'rgb(218, 41, 28)', width = 1)) %>%
   add_trace(y = ~Newcastle, name = 'Newcastle', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(45, 41, 38)', width = 1, dash = 'dash')) %>%
-  # add_trace(y = ~Norwich, name = 'Norwich City', type = 'scatter', mode = 'lines', 
-  #           line = list(shape = 'spline', color = 'rgb(0, 166, 80)', width = 1, dash = 'dash')) %>%
-  add_trace(y = ~SheffieldUnited, name = 'Sheffield United', type = 'scatter', mode = 'lines', 
-            line = list(shape = 'spline', color = 'rgb(238,39,55)', width = 1, dash = 'dash')) %>%
+  add_trace(y = ~Norwich, name = 'Norwich City', type = 'scatter', mode = 'lines',
+            line = list(shape = 'spline', color = 'rgb(0, 166, 80)', width = 1, dash = 'dash')) %>%
+  # add_trace(y = ~SheffieldUnited, name = 'Sheffield United', type = 'scatter', mode = 'lines', 
+  #           line = list(shape = 'spline', color = 'rgb(238,39,55)', width = 1, dash = 'dash')) %>%
   add_trace(y = ~Southampton, name = 'Southampton', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(215, 25, 32)', width = 1, dash = 'dash')) %>%
   add_trace(y = ~Tottenham, name = 'Tottenham', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(19, 34, 87)', width = 1)) %>%
-  # add_trace(y = ~Watford, name = 'Watford', type = 'scatter', mode = 'lines', 
-  #           line = list(shape = 'spline', color = 'rgb(251, 238, 35)', width = 1)) %>%
-  add_trace(y = ~WestBrom, name = 'West Bromwich Albion', type = 'scatter', mode = 'lines', 
-            line = list(shape = 'spline', color = 'rgb(18, 47, 103)', width = 1)) %>%
+  add_trace(y = ~Watford, name = 'Watford', type = 'scatter', mode = 'lines',
+            line = list(shape = 'spline', color = 'rgb(251, 238, 35)', width = 1)) %>%
+  # add_trace(y = ~WestBrom, name = 'West Bromwich Albion', type = 'scatter', mode = 'lines', 
+  #           line = list(shape = 'spline', color = 'rgb(18, 47, 103)', width = 1)) %>%
   add_trace(y = ~WestHam, name = 'West Ham United', type = 'scatter', mode = 'lines', 
             line = list(shape = 'spline', color = 'rgb(122, 38, 58)', width = 1)) %>%
   add_trace(y = ~Wolves, name = 'Wolverhampton', type = 'scatter', mode = 'lines', 
