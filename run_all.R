4f27ec076593e5b3f368aed7907ba21906819163
Nicholas Pylypiw <nick.pylypiw@gmail.com>
2024-02-15 22:55:48 -0500

    renamed stuff and commented other stuff out

--- run_all.R
+++ run_all.R
@@ -1,12 +1,12 @@
 source("data/data_preparation.R")
 
 source("visuals/rank_by_year.R")
-source("visuals/points_by_week_10yr.R")
+source("visuals/points_by_week_champions.R")
 
 source("visuals/rank_by_week.R")
 source("visuals/points_by_week.R")
-source("visuals/animated_points_by_week.R")
-source("visuals/predicted_points_by_week.R")
+# source("visuals/animated_points_by_week.R")
+# source("visuals/predicted_points_by_week.R")
 
 source("visuals/goal_diff_by_week.R")
 source("visuals/goal_differential_by_team.R")
@@ -16,13 +16,14 @@ source("visuals/goals_conceded_by_team.R")
 source("visuals/opp_rank_remaining.R")
 
 rank.by.year
-points.by.week.10yr
+points.by.week.champions
 rank.by.week
 points.by.week
-animated.points.by.week
-predicted.points.by.week
+# animated.points.by.week
+# predicted.points.by.week
 goal.diff.by.week
 goal.differential.by.team
 goals.scored.by.team
 goals.conceded.by.team
 opp.rank.by.week
+
